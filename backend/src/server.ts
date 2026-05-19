185c042d5aba59483cfe788d0b7b77616206b1ac
home-mia <ydjvchslf@gmail.com>
2026-05-20 00:39:02 +0200

    Wöchentliche Top-10-Filmliste geprüft

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,50 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 const PORT = 4000; // Server port: 4000
+const TMDB_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDAxYjQzNzQ3MjJmNjJkOWEzNDYyZTAxNmZiNjEwNyIsIm5iZiI6MTc3OTIyNjkyOC43OTgsInN1YiI6IjZhMGNkOTMwYTY1ODMwOTVlZTA2OWI1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uY-Nij6Itq0E3s1DMJ76XY-p38TcqSfJ5aqdZZKhbN8';
+
+app.use(cors());
 
 // 💡 [API 창구 1] 프론트엔드가 'http://localhost:4000/'로 노크하면 반응하는 곳
-app.get('/', (req: Request, res: Response) => {
-  console.log("Jemand ist in die Address reingekommen! 야 되나?");
-  res.send('Movie App Backend Server gut funcktiniert! 🚀');
+app.get('/', async (req: Request, res: Response) => {
+  console.log("Jemand ist in die Address reingekommen! Nodemon Überwachung!");
+
+  // Get the trending movies on TMDB. (Wochendlich)
+  try {
+    // A. axios 배달원에게 TMDB 본사 주소로 가서 이번 주 트렌드 영화 가져오라고 명령!
+    const response = await axios.get('https://api.themoviedb.org/3/trending/movie/week', { // day oder week Quary 수정가능
+      headers: {
+        accept: 'application/json',
+        Authorization: TMDB_TOKEN // 스마트 카드키 가방에 넣어서 보냄
+      },
+      params: {
+        language: 'ko-KR' // ⭐ 한국어 제목과 줄거리로 받아오는 마법의 옵션!
+      }
+    });
+
+    // B. TMDB가 보내준 수많은 영화 목록(results) 중에서 상위 10개만 싹둑 자릅니다.
+    const top10Movies = response.data.results.slice(0, 10);
+
+    // C. 예쁘게 가공된 진짜 데이터 10개를 프론트엔드(리액트)로 당당하게 던져줍니다!
+    return res.json({
+      success: true,
+      data: top10Movies
+    });
+
+  } catch (error: any) {
+    console.error('⚠️ TMDB 데이터 가져오기 실패:', error.message);
+    return res.status(500).json({
+      success: false,
+      message: 'TMDB 서버 통신 중 에러가 발생했습니다.'
+    });
+  }
+
+  res.send('Movie App Backend Server gut funcktiniert! 🚀'); // response 끝!
+
 });
 
 // 💡 [API 창구 2] 나중에 진짜 10개 영화 데이터를 던져줄 가상의 창구
